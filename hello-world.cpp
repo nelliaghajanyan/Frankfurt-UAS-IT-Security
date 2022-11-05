3d828ed9521506da0475b02beffff272bbca4ddf
nelliaghajanyan <aghajanyannelli@gmail.com>
2022-11-05 19:24:21 +0400

    Add files via upload

---
+++ hello-world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World\n";
+    cout<<"Created for HIS Project - Evaluation of IT-Security Problems.\n";
+    cout<<"Good luck!";
+    
+    
+    return 0;
+}
